f5ba33843ed7f2548d05c7e2df46076c836e0c0a
XIANGKE GUO <mbp@XIANGKEs-MacBook-Pro.local>
2024-05-12 05:06:16 -0700

    add live and studio

--- app/Resources/Resources/Youtubes.php
+++ app/Resources/Resources/Youtubes.php
@@ -13,9 +13,13 @@ final class Youtubes{
     // https://www.youtube.com/watch?v=Y8X8JXNbBbI&list=RDwwpK3p4heEM&index=2
     // https://m.youtube.com/shorts/6vsRGl4IsMs?si=QBA11T3v1Sl6ijxo
     // https://www.youtube.com/shorts/6vsRGl4IsMs?app=desktop
+    // https://www.youtube.com/watch?app=desktop&v=wk2JytIsAz8&si=3CXuY_JFQsMkj_ku
+    // https://studio.youtube.com/video/zG0eTyLMxlM/edit
+    // https://www.youtube.com/live/AarqK67LPfE?si=ORsJMYYsxyQYTMB8
+
 	public function _invoke($keyword)
 	{
-        if(preg_match('/(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=)|youtube\.com\/shorts\/)([\w\-]{11})/', $keyword, $matches)){
+        if(preg_match('/(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=|video\/|shorts\/|live\/))([\w\-]{11,})/', $keyword, $matches)){
             $vid = $matches[1];
             $videoInfo = Youtube::getVideoInfo($vid);
             $title = $videoInfo->snippet->title;
