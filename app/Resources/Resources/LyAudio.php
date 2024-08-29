455f336cffd4ffd5760d7135aad5400261353b68
me@mbp.m3 <TWR-****-****-****-7F47>
2024-08-28 21:33:17 -0700

    no

--- app/Resources/Resources/LyAudio.php
+++ app/Resources/Resources/LyAudio.php
@@ -137,7 +137,7 @@ EOD;
                 $data =[
                     'type' => 'music',
                     'data' => [
-                        "url" => str_replace('https://open.savefamily.today','https://d3ml8yyp1h3hy5.cloudfront.net',$item['link']),
+                        "url" => $item['link'],
                         'title' => "【{$keyword}】".str_replace('圣经','SJ',$item['program_name']).'-'.$item['play_at'],
                         'description' => str_replace('教会','JH',$item['description']),
                         'image' => "https://txly2.net/images/program_banners/{$code}_prog_banner_sq.png",
