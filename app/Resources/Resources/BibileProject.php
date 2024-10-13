aff3fe64bc45fe4f68f8e8b7523b4fa2e3a5945f
me@mbp.m3 <TWR-****-****-****-7F47>
2024-10-13 08:23:52 -0700

    7830

--- app/Resources/Resources/BibileProject.php
+++ app/Resources/Resources/BibileProject.php
@@ -11,7 +11,31 @@ use voku\helper\HtmlDomParser;
 final class BibileProject{
 	public function _invoke($keyword)
 	{
-        // 783 bibleproject 106!
+        // 783 bibleproject 106! =》115！
+        if($keyword == 7830){
+            $cacheKey = "xbot.keyword.7830";
+            $data = Cache::get($cacheKey, false);
+            if(1||!$data){
+                // https://miniature-calendar.com/221003
+                $response = Http::get("https://bibleproject.com/locale/downloads/zhs/");
+                $html =$response->body();
+                // $htmlTmp = HtmlDomParser::str_get_html($html);
+                $re = '/<div class="intl-downloads-item-title">(.*?)<\/div>[\s\S]*?<a\s+href="(https:\/\/[^"]+\.mp4)"/m';
+                // $re = '/<div class="intl-downloads-item-title">([^<]+)<\/div>.*?<a href="([^"]+\.mp4)".*?(?:<a href="([^"]+\.png)")?/s';
+                $pattern = '/<div class="intl-downloads-item-title">([^<]+)<\/div>.*?<a\s+href="([^"]+\.mp4)"[^>]*>.*?(?:<a\s+href="([^"]+\.png)"[^>]*>[^<]*<\/a>|<span class="intl-downloads-item-empty">无<\/span>)/s';
+
+                preg_match_all($pattern, $html, $data);
+                unset($data[0]);
+                // Cache::put($cacheKey, $data, strtotime('+1 week') - time());
+            }
+            $total = count($data[1]);
+            $offset = (now()->format('z')+10)%$total+1;
+            
+            $title = $data[1];
+            $mp4 = $data[2];
+            $png = $data[3];
+            return $mp4;//compact('title','mp4','png');
+        }
         if($keyword == "bibleproject"||$keyword == 783){
             $date = now()->format('ymd');
             $cacheKey = "xbot.keyword.bibileproject";
