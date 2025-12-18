a7d67c4a1e8a77b5cdbcd699f19372b62d07e195
me@mbp.m3 <TWR-****-****-****-7F47>
2025-12-17 16:50:19 -0800

    ly image

--- app/Resources/Resources/LyAudio.php
+++ app/Resources/Resources/LyAudio.php
@@ -134,14 +134,14 @@ EOD;
                 $url = str_replace('https://x.lydt.work/storage/','https://d3ml8yyp1h3hy5.cloudfront.net/',$item['link']);
                 if(in_array($keyword,[641,642,643,644,645]))
                     $url = str_replace('/ly/audio/','/lts/', $url);
-                $image = "https://dtly.work/images/program_banners/{$code}_prog_banner_sq.png";
+                $image = "https://txly2.net/images/program_banners/{$code}_prog_banner_sq.png";
                 $data =[
                     'type' => 'music',
                     'data' => [
                         "url" => $url,
                         'title' => "【{$keyword}】".str_replace('圣经','SJ',$item['program']['name']).' '.substr($item['play_at'],5,5),
                         'description' => str_replace('教会','JH',$item['description']),
-                        'image' => $image,
+                        'image' => "",
                     ],
                 ];
                 $data['statistics'] = [
